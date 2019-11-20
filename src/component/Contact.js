415ac15b3b9f058ae3ba8c91b1f1d0b1b9d7c84f
Kevin Earle Cruz <k.earle.dc@gmail.com>
2019-11-20 16:36:40 +0800

    Tuloy sa Bahay

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Columns } from "react-bulma-components";
+import { Container, Columns, Heading, Box } from "react-bulma-components";
 import './../App.css';
 
 
@@ -41,7 +41,9 @@ const Contact = () => {
 
 				{/*Map*/}
 				<Columns.Column>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.4101322928072!2d121.04148921484061!3d14.632645089782075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b7afe6a39331%3A0xd367e6dc5e274032!2sCaswynn%20Building%2C%20134%20Timog%20Ave%2C%20Diliman%2C%20Quezon%20City%2C%201103%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1574221603242!5m2!1sen!2sph" frameborder="0" style={{border:0}} id="gmap"></iframe>
+					<Box>
+					<Heading color="dark"> Let's Work Together?</Heading>
+					</Box>
 				</Columns.Column>
 			</Columns>
 		</Container>
